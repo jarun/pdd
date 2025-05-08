2928809ee4700982e221f46fb15fa014f3e9e598
Marius Hoch <mail@mariushoch.de>
2025-05-08 18:17:45 +0200

    auto-completion: bash: Also complete opts_with_arg
    Also some minor clean ups that shouldn't change behavior.

--- auto-completion/bash/pdd.bash
+++ auto-completion/bash/pdd.bash
@@ -7,14 +7,7 @@ _pdd () {
     COMPREPLY=()
     local IFS=$' \n'
     local cur=$2 prev=$3
-    local -a opts opts_with_args
-    opts=(
-        --add
-        -h --help
-        -q --quiet
-        --sub
-        -v --version
-    )
+    local -a opts opts_with_arg
     opts_with_arg=(
         -c --timer
         -d --date
@@ -23,13 +16,21 @@ _pdd () {
         -s --stopwatch
         -t --time
     )
+    opts=(
+        "${opts_with_arg[@]}"
+        --add
+        -h --help
+        -q --quiet
+        --sub
+        -v --version
+    )
 
     # Do not complete non option names
     [[ $cur == -* ]] || return 1
 
     # Do not complete when the previous arg is an option expecting an argument
     for opt in "${opts_with_arg[@]}"; do
-        [[ $opt == $prev ]] && return 1
+        [[ "$opt" == "$prev" ]] && return 1
     done
 
     # Complete option names
